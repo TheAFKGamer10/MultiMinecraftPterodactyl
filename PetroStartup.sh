c5ce6aa89b2c101ba8da35178eec21248a1d6ba7
TheAFKGamer10 <gumballguy12@gmail.com>
2024-06-19 14:36:16 -0500

    Fix Caps

--- PetroStartup.sh
+++ PetroStartup.sh
@@ -8,9 +8,9 @@ cd /mnt/server
 rm -f start.sh
 rm -f install.txt
 wget https://raw.githubusercontent.com/TheAFKGamer10/MultiMinecraftPterodactyl/main/Install.sh
-chmod -x install.sh
-bash install.sh > install.txt
-rm -f install.sh
+chmod -x Install.sh
+bash Install.sh > install.txt
+rm -f Install.sh
 
 echo -e "Installation Complete"
 exit 0
